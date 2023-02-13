a66e7f41ddeaa6689edbd0ec8f8ad07dc0b31554
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-02-13 14:00:02 -0300

    feat: Criação do route de get dos users.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
-
+import userRoutes from "./routes/users.routes";
 const app = express();
 
 app.use(express.json());
+
+app.use('/users',userRoutes)
+
+
 app.listen(3000, () => {
 
 
